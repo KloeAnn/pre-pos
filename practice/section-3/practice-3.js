7a7e85aae3671001295635dd1e4ff09ae3688cea
KloeAnn <201530611012@mail.scut.edu.cn>
2019-07-08 22:56:37 +0800

    done

--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,5 +1,32 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var res=[]
+  for(var i=0;i<collectionA.length;i++){
+    if(i==0){
+      res.push({key:collectionA[i],count:1})
+      console.log("aa"+" "+res)
+    }else {
+      for(var j=0;j<res.length;j++){
+        if(collectionA[i]==res[j].key){
+          res[j].count++
+          break
+        }else{
+          if(j==res.length-1){
+            res.push({key:collectionA[i],count:1})
+            break
+          }
+        }
+      }
+      console.log("bb"+" "+collectionA[i])
+    }
+  }
+  for(var i=0;i<res.length;i++){
+    for(var j=0;j<objectB.value.length;j++){
+      if(res[i].key==objectB.value[j]){
+        res[i].count-=Math.floor(res[i].count/3)
+      }
+    }
+  }
+  return res
 }
--- practice/section-3/practice-4.js
+++ practice/section-3/practice-4.js
@@ -1,5 +1,48 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var res=[]
+  var c
+  for(var i=0;i<collectionA.length;i++){
+    if(i==0){
+      if(collectionA[i].indexOf("-")==-1)
+        res.push({key:collectionA[i],count:1})
+      else {
+        res.push({key:collectionA[i].split("-")[0],
+          count:collectionA[i].split("-")[1]})
+      }
+    }else {
+      for(var j=0;j<res.length;j++){
+        if(collectionA[i]==res[j].key){
+          if(collectionA[i].indexOf("-")==-1)
+            res[j].count++
+          else {
+            c=parseInt(collectionA[i].split("-")[1])
+            res[j].count+=c
+          }
+          break
+        }else{
+          if(j==res.length-1){
+            if(collectionA[i].indexOf("-")==-1)
+              res.push({key:collectionA[i],count:1})
+            else {
+              c=parseInt(collectionA[i].split("-")[1])
+              res.push({key:collectionA[i].split("-")[0],
+                count:c})
+            }
+            break
+          }
+        }
+      }
+    }
+  }
+  for(var i=0;i<res.length;i++){
+    for(var j=0;j<objectB.value.length;j++){
+      if(res[i].key==objectB.value[j]){
+        res[i].count-=Math.floor(res[i].count/3)
+      }
+    }
+  }
+  console.log(res)
+  return res
 }
